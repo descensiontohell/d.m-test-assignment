bacfd873ead46ab24ecc4b301dda754d703acc37
descensiontohell <isuhanen@gmail.com>
2022-09-17 22:02:10 +0300

    ops: add nginx configuration with static resources handling

--- prestart.sh
+++ prestart.sh
@@ -2,4 +2,15 @@
 sleep 3
 cd gallery_backend
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
+
+$@
+
+gunicorn gallery_backend.wsgi:application --bind 0.0.0.0:8000
