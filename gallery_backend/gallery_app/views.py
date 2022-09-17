57844c5075e9066506fb2b6a62d5c8095298b2b6
descensiontohell <isuhanen@gmail.com>
2022-09-17 12:52:56 +0300

    refactor: reworked views

--- gallery_backend/gallery_app/views.py
+++ gallery_backend/gallery_app/views.py
@@ -1,60 +1,56 @@
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from rest_framework import status
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework import views
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.response import Response
-
 
 from .models import Image
 from .serializers import CurrentUserSerializer, ImageSerializer, UserSerializer, UserImagesSerializer
+from .permissions import IsOwnerOrReadOnly, IsAdminOrReadOnly
 
 
-class ImagesViewSet(ModelViewSet):
+class AdminImageView(views.APIView):
     permission_classes = [IsAdminUser]
-    queryset = Image.objects.all()
-    serializer_class = ImageSerializer
+
+    def get(self, request, *args, **kwargs):
+        images = Image.objects.all()
+        serializer = ImageSerializer(images, many=True)
+        return Response(serializer.data)
+
+    def put(self, request, *args, **kwargs):
+        return Response()
+
+    def delete(self, request, *args, **kwargs):
+        Image.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class UsersViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer  # TODO отключить круды всем кроме админа
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 
-class CurrentUserViewSet(ReadOnlyModelViewSet):
+class ImageViewSet(viewsets.ModelViewSet):
+    serializer_class = ImageSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        return Image.objects.filter(user=self.kwargs["user_pk"])
+
+    def create(self, request, *args, **kwargs):
+        request.data["user"] = self.request.user.id
+        return super().create(request, *args, **kwargs)
+
+
+class CurrentUserView(viewsets.ReadOnlyModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = CurrentUserSerializer
 
     def list(self, request, *args, **kwargs):
         user = request.user
         serializer = self.get_serializer(user)
-        return Response(serializer.data)
-
-
-class UserImagesViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
-
-    def list(self, *args, **kwargs):
-        user_id = kwargs["user_id"]
-        queryset = Image.objects.filter(user_id=user_id)
-        serializer = UserImagesSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def create(self, *args, **kwargs):
-        ...  # TODO сделать круды для картинок
-
-
-class UserSingleImageViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
-
-    def list(self, *args, **kwargs):
-        user_id = kwargs["user_id"]
-        image_id = kwargs["image_id"]
-
-        # try:
-        queryset = Image.objects.get_object_or_404(user_id=user_id, id=image_id)
-        # except Image.DoesNotExist:
-        #     return Response(status=status.HTTP_404_NOT_FOUND, data={"detail": "Not found."})
-
-        serializer = UserImagesSerializer(queryset, many=False)
         return Response(serializer.data)
