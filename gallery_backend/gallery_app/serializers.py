cebcfe2c10d6f00a530adb25cfbdfd6cc41932dd
descensiontohell <isuhanen@gmail.com>
2022-09-17 12:53:14 +0300

    refactor: reworked serializers

--- gallery_backend/gallery_app/serializers.py
+++ gallery_backend/gallery_app/serializers.py
@@ -1,33 +1,27 @@
 from django.contrib.auth.models import User
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, ReadOnlyField
 from .models import Image
 
 
-class ImageSerializer(serializers.ModelSerializer):
+class ImageSerializer(ModelSerializer):
     class Meta:
         model = Image
         fields = "__all__"
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "username")
+        fields = ("id", "username", "email")
 
 
-class CurrentUserSerializer(serializers.ModelSerializer):
+class CurrentUserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ("id", "username", "email", "last_login")
 
 
-class UserImagesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Image
-        fields = ("id", "image_url", "created_at")
-
-
-class UserSingleImageSerializer(serializers.ModelSerializer):
+class UserImagesSerializer(ModelSerializer):
     class Meta:
         model = Image
         fields = ("id", "image_url", "created_at")
