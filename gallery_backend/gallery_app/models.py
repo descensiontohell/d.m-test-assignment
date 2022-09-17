74620f3efcb93e1f4aecc691fb541d1e33bc2d55
descensiontohell <isuhanen@gmail.com>
2022-09-17 12:53:57 +0300

    fix: add missing auto datetime.now() on object createa

--- gallery_backend/gallery_app/models.py
+++ gallery_backend/gallery_app/models.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 class Image(models.Model):
     id = models.AutoField(primary_key=True)
     image_url = models.URLField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField()
+    user = models.ForeignKey(User, related_name="images", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
